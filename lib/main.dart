9c302ae25e8dd6724fc199cfd876faafb4943329
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-03-23 00:04:53 -0300

    criado o seção dos seguidores

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class MyHomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          //top section
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
@@ -77,6 +78,56 @@ class MyHomePage extends StatelessWidget {
                 ),
               )
             ],
+          ),
+          //section seguidores
+          Padding(
+            padding: const EdgeInsets.only(right: 38.0, left: 38, top: 15, bottom: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('102k', style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25
+                    )),
+                    Text('Seguidores', style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+                Container(
+                    color: Colors.white,
+                    width: 0.3,
+                    height: 22,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('30k', style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25
+                    )),
+                    Text('Seguidores', style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+                Container(
+                    color: Colors.white,
+                    width: 0.3,
+                    height: 22,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 18, right: 18, top: 8, bottom: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(33)),
+                    gradient: LinearGradient(colors: [Color(0xff6D0EB5), Color(0xff4059F1)],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.centerLeft),
+                  ),
+                  child: Text('Seguir', style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  )),
+                )
+              ],
+            ),
           )
         ],
       ),
